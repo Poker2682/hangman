8f7e311164c2f5d25eac76bb8cb2a9fa7330f1a9
Daniel Montes <daniel.montes2682@gmail.com>
2023-06-20 14:41:03 -0500

    Centrado

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -27,71 +27,77 @@ class _GameScreenState extends State<GameScreen> {
         elevation: 0.0,
         backgroundColor: Colors.transparent,
       ),
-      body: Column(
-        children: [
-          Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Expanded(
-                      flex: 4,
-                      child: Stack(children: [
-                        figure(GameUI.hang, tries >= 0),
-                        figure(GameUI.head, tries >= 1),
-                        figure(GameUI.body, tries >= 2),
-                        figure(GameUI.leftArm, tries >= 3),
-                        figure(GameUI.rightArm, tries >= 4),
-                        figure(GameUI.leftLeg, tries >= 5),
-                        figure(GameUI.rightLeg, tries >= 6),
-                      ])),
-                  Expanded(
-                      child: Container(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 4,
+                          child: Stack(children: [
+                            figure(GameUI.hang, tries >= 0),
+                            figure(GameUI.head, tries >= 1),
+                            figure(GameUI.body, tries >= 2),
+                            figure(GameUI.leftArm, tries >= 3),
+                            figure(GameUI.rightArm, tries >= 4),
+                            figure(GameUI.leftLeg, tries >= 5),
+                            figure(GameUI.rightLeg, tries >= 6),
+                          ])),
+                      Expanded(
+                          child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: word
+                              .split("")
+                              .map((e) => hiddenLetter(
+                                  e, selectedChar.contains(e.toUpperCase())))
+                              .toList(),
+                        ),
+                      ))
+                    ],
+                  )),
+              Expanded(
+                  flex: 2,
+                  child: Container(
                     padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: word
+                    child: GridView.count(
+                      physics: const NeverScrollableScrollPhysics(),
+                      crossAxisCount: 7,
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4,
+                      children: characters
                           .split("")
-                          .map((e) => hiddenLetter(
-                              e, selectedChar.contains(e.toUpperCase())))
+                          .map((e) => ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black54),
+                              onPressed: selectedChar.contains(e.toUpperCase())
+                                  ? null
+                                  : () {
+                                      setState(() {
+                                        selectedChar.add(e.toUpperCase());
+                                        if (!word
+                                            .split("")
+                                            .contains(e.toUpperCase())) {
+                                          tries++;
+                                        }
+                                      });
+                                    },
+                              child: Text(
+                                  style: const TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold),
+                                  e)))
                           .toList(),
                     ),
-                  ))
-                ],
-              )),
-          Expanded(
-              flex: 2,
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                child: GridView.count(
-                  physics: const NeverScrollableScrollPhysics(),
-                  crossAxisCount: 7,
-                  crossAxisSpacing: 4,
-                  mainAxisSpacing: 4,
-                  children: characters
-                      .split("")
-                      .map((e) => ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black54),
-                          onPressed: selectedChar.contains(e.toUpperCase())
-                              ? null
-                              : () {
-                                  setState(() {
-                                    selectedChar.add(e.toUpperCase());
-                                    if (!word
-                                        .split("")
-                                        .contains(e.toUpperCase())) {
-                                      tries++;
-                                    }
-                                  });
-                                },
-                          child: Text(
-                              style: const TextStyle(
-                                  fontSize: 22, fontWeight: FontWeight.bold),
-                              e)))
-                      .toList(),
-                ),
-              )),
-        ],
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
