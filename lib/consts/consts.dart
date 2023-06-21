d4e345f71bb74c76620cbbab56566457e6975bbd
Daniel Montes <daniel.montes2682@gmail.com>
2023-06-21 16:19:05 -0500

    Ajustes

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const bgColor = Color(0xff2c3333);
   static const textColor = Color(0xffe7f6f2);
+  static const letterBgColor1 = Color.fromARGB(255, 252, 246, 240);
+  static const letterBgColor2 = Color.fromARGB(255, 216, 168, 133);
 }
 
 class GameUI {
--- lib/letter/letter_screen.dart
+++ lib/letter/letter_screen.dart
@@ -1,33 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:hangman/consts/consts.dart';
 
 class LetterScreen extends StatelessWidget {
+  static const List images = [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+  ];
+
   const LetterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-          child: Column(
-        children: [
-          Expanded(flex: 2, child: Image.asset("assets/love-img-1.png")),
-          const Expanded(
-              child: Text(
-                  style: TextStyle(fontSize: 28, color: Colors.black87),
-                  "Worka bonito")),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(200, 50),
-                maximumSize: const Size(200, 50),
-                backgroundColor: Colors.red),
-            child: const Text("Siguiente"),
-            onPressed: () => {},
-          ),
-          const SizedBox(
-            height: 50,
-          )
-        ],
-      )),
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: index == 0
+                      ? AppColors.letterBgColor1
+                      : AppColors.letterBgColor2,
+                  image: DecorationImage(
+                    image: AssetImage("assets/letter/${images[index]}"),
+                  )),
+            );
+          }),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Colors.white,
+  //     body: Center(
+  //         child: Column(
+  //       children: [
+  //         Expanded(flex: 2, child: Image.asset("assets/love-img-1.png")),
+  //         const Expanded(
+  //             child: Text(
+  //                 style: TextStyle(fontSize: 28, color: Colors.black87),
+  //                 "Worka bonito")),
+  //         ElevatedButton(
+  //           style: ElevatedButton.styleFrom(
+  //               minimumSize: const Size(200, 50),
+  //               maximumSize: const Size(200, 50),
+  //               backgroundColor: Colors.red),
+  //           child: const Text("Siguiente"),
+  //           onPressed: () => {},
+  //         ),
+  //         const SizedBox(
+  //           height: 50,
+  //         )
+  //       ],
+  //     )),
+  //   );
+  // }
 }
